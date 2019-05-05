5aec2fb28797f2c6b53b42815235008df2cc8c55
LeTranKhoa <khoalt.pht@gmail.com>
2019-05-06 01:46:05 +0700

    vayHi method for Dog

--- Dog.js
+++ Dog.js
@@ -1,7 +1,14 @@
-function Dog(){
+var chalk = require('chalk');
+
+function Dog(name){
 	this.tomach =[];
-	
+	this.name = name;
 }
 Dog.prototype.eat = function(cat){
 	this.stomach.push(cat)
 };
+Dog.prototype.sayHi = function(){
+	console.log('Hi! im Dog .My name is' + chalk.red(this.name));
+};
+
+module.exports = Dog;
